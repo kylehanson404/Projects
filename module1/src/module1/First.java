f2e5c7a01e641dbde065b86abae7a980061dd732
kylehanson404 <hansonkyle484@gmail.com>
2026-01-26 15:25:10 -0500

    Create First.java
    initial code

---
+++ module1/src/module1/First.java
@@ -0,0 +1,14 @@
+package module1;
+
+//Kyle Hanson
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!");
+		System.out.println("Job title: Data Scientist");
+		System.out.println("Why: Good job prospects");
+		System.out.println("Median salary: $125,000");
+	}
+
+}
