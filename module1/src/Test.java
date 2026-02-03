5e1329b2b17f93a4ae107376f7a10815ac17faec
kylehanson404 <hansonkyle484@gmail.com>
2026-02-02 21:51:50 -0500

    Create Test.java

---
+++ module1/src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declare and initialize test score variables
+		double testScore1 = 88.2;
+		double testScore2 = 78.9;
+		double testScore3 = 97.6;
+
+		// Calculate the average
+		double average = (testScore1 + testScore2 + testScore3) / 3;
+
+		// Display the test scores and average
+		System.out.println("Test score 1:   " + testScore1);
+		System.out.println("Test score 2:  " + testScore2);
+		System.out.println("Test score 3:  " + testScore3);
+		System.out.println("The average of 3 test scores is:  " + average);
+	}
+
+}
